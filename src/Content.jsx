10de569e0b3d39263d5ec78b6df9e9d1ccfc58a7
brian <riceb53@gmail.com>
2023-09-05 09:40:02 -0700

    render data from react, no web request yet

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,57 @@
+import { useState } from "react"
+import { ProductsIndex } from './ProductsIndex'
+
 export function Content() {
+  const [products, setProducts] = useState([
+    {
+      "id": 8,
+      "name": "water bottleeeeee",
+      "price": "10.0",
+      "description": "holds water",
+      "is_discounted?": true,
+      "tax": "0.9",
+      "total": "10.9",
+      "supplier": null,
+      "images": [],
+      "categories": []
+    },
+    {
+      "id": 9,
+      "name": "sunglasses",
+      "price": "10.0",
+      "description": "holds water",
+      "is_discounted?": true,
+      "tax": "0.9",
+      "total": "10.9",
+      "supplier": null,
+      "images": [],
+      "categories": [
+        {
+          "id": 1,
+          "name": "beach",
+          "created_at": "2023-08-11T16:25:09.370Z",
+          "updated_at": "2023-08-11T16:25:09.370Z"
+        },
+        {
+          "id": 3,
+          "name": "home",
+          "created_at": "2023-08-11T16:25:57.468Z",
+          "updated_at": "2023-08-11T16:25:57.468Z"
+        }
+      ]
+    }
+  ])
+  
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <ProductsIndex products={products} />
     </div>
   )
 }
+
+// make a react variable
+// make a web request to rails (in a function) to get the right data
+// call the function to make the web request
+// reassign the variable to the data that comes back from rails
+// pass that data into productsIndex to render that data
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,19 @@
+export function ProductsIndex(props) {
+  console.log(props.products)
+  
+  return (
+    <div>
+      <p>Hello from photos index</p>
+      {props.products.map(product => (
+        <div key={product.id}>
+          <p>{product.name}</p>
+        </div>
+        ))}
+    </div>
+
+  )
+}
+
+
+// accept props from content
+// show those props using a .map to the user
